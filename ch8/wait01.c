d2b44bb6f99942bfe1b964baf2dc5f9e3227ab00
Ji Hong Geun <jihonggeun@Jis-MacBook-Air.local>
2020-11-09 15:42:56 +0900

    implement wait example

--- ch8/wait01.c
+++ ch8/wait01.c
@@ -17,16 +17,24 @@ int main()
 {
     pid_t pid;
     int status;
-    
+
     if ((pid = fork()) < 0) err_sys("fork error");
     else if (pid == 0) exit(7);
-    
+
     if (wait(&status) != pid) err_sys("wait error");
     pr_exit(status);
-    
+
     if ((pid = fork()) < 0) err_sys("fork error");
-    else if (pid == 0) abort();
-    
+    else if (pid == 0) abort();   // SIGABRT signal
+
     if (wait(&status) != pid) err_sys("wait error");
-    
+    pr_exit(status);
+
+    if ((pid = fork()) < 0) err_sys("fork error");
+    else if (pid == 0) status /= 0; // SIGFPE signal
+
+    if (wait(&status) != pid) err_sys("wait error");
+    pr_exit(status);
+
+    exit(0);
 }
