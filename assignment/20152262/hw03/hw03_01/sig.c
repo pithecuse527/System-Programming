3b59c840f36063da49610966c3b6ea3c117f890e
d_PITHECUSE_b <gihong96@gmail.com>
2020-11-26 02:13:17 +0900

    first version for hw03_01

--- assignment/20152262/hw03/hw03_01/sig.c
+++ assignment/20152262/hw03/hw03_01/sig.c
@@ -9,36 +9,29 @@ void sig_handler2(int signo);
 
 int main(int argc, char *argv[])
 {
-  printf("%s\n", argv[1]);
   if(argc > 2)
   {
     fprintf(stderr, "too many arguments...");
     exit(-1);
   }
 
+  strcpy(current_sig, argv[1]);
+  fprintf(stdout, "\n");
+  fprintf(stdout, "Start [%s] - %d", current_sig, (int)getpid());
+  fprintf(stdout, "\n");
+
   if(signal(SIGUSR1, (void*)sig_handler2) == SIG_ERR)
   {
-    fprintf(stderr, "can't catch SIGUSR1\n");
+    fprintf(stderr, "can't catch SIGUSR1");
     exit(-1);
   }
-  if(signal(SIGUSR1, (void*)sig_handler2) == SIG_ERR)
+  if(signal(SIGUSR2, (void*)sig_handler2) == SIG_ERR)
   {
-    fprintf(stderr, "can't catch SIGUSR2\n");
+    fprintf(stderr, "can't catch SIGUSR2");
     exit(-1);
   }
 
-  if(strcmp(argv[1], "sigint") == 0)
-  {
-    strcpy(current_sig, "sigint");
-    fprintf(stdout, "Start [%s] - %d", current_sig, (int)getpid());
-  }
-  else if(strcmp(argv[1], "sigtstp") == 0)
-  {
-    strcpy(current_sig, "sigtstp");
-    fprintf(stdout, "Start [%s] - %d", current_sig, (int)getpid());
-  }
-  sleep(1);
-  // for (;;) pause();   // wait for my signal
+  for(;;) pause();   // wait for my signal
 
 }
 
@@ -47,13 +40,17 @@ void sig_handler2(int signo)
   switch(signo)
   {
     case SIGUSR1:
+      fprintf(stdout, "\n");
       fprintf(stdout, "Running [%s] - %d", current_sig, (int)getpid());
+      fprintf(stdout, "\n");
       break;
     case SIGUSR2:
+      fprintf(stdout, "\n");
       fprintf(stdout, "Exit [%s] - %d", current_sig, (int)getpid());
+      fprintf(stdout, "\n");
       exit(0);
     default:
-      fprintf(stdout, "unknown signal for sig_handler2\n");
+      fprintf(stdout, "unknown signal for sig_handler2");
       exit(-1);
   }
 }
--- assignment/20152262/hw03/hw03_01/src.c
+++ assignment/20152262/hw03/hw03_01/src.c
@@ -3,11 +3,15 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#define MAX_PROCESS_AMOUNT 20
 
 void sig_handler(int signo);
+pid_t childs[MAX_PROCESS_AMOUNT];
+int i;
 
 int main()
 {
+
   if(signal(SIGINT, (void*)sig_handler) == SIG_ERR)
   {
     fprintf(stderr, "can't catch SIGINT\n");
@@ -30,51 +34,60 @@ int main()
   }
 
   for (;;) pause();   // wait for my signal
-  exit(0);
+
 }
 
 void sig_handler(int signo)
 {
+  int j;
   pid_t pid;
-  if((pid=fork())<0) fprintf(stderr, "fork error");
-  else if(pid == 0)
+  switch(signo)
   {
-    switch(signo)
-    {
-      case SIGINT:
+    case SIGINT:
+      if((pid = fork()) < 0) fprintf(stderr, "fork error\n");
+      else if(pid == 0)
+      {
         if(execl("./sig", "sig", "sigint", (char *)0) < 0)
         {
           fprintf(stderr, "execl error on SIGINT\n");
           exit(-1);
         }
-        printf("!!%d!!\n", signo);
-        break;
-      case SIGTSTP:
+      }
+      else childs[i++] = pid;
+      break;
+    case SIGTSTP:
+      if((pid = fork()) < 0) fprintf(stderr, "fork error\n");
+      else if(pid == 0)
+      {
         if(execl("./sig", "sig", "sigstp", (char *)0) < 0)
         {
           fprintf(stderr, "execl error on SIGINT\n");
           exit(-1);
         }
-        break;
-      case SIGUSR1:
-        if(kill(0, SIGUSR1) < 0)
+      }
+      else childs[i++] = pid;
+      break;
+    case SIGUSR1:
+      for(j=0;j<=i;j++)
+      {
+        if(kill(childs[j], SIGUSR1) < 0)
         {
           fprintf(stderr, "kill error on SIGUSR1\n");
           exit(-1);
         }
-        break;
-      case SIGUSR2:
-        if(kill(0, SIGUSR2) < 0)
-        {
-          fprintf(stderr, "kill error on SIGUSR2\n");
-          exit(-1);
-        }
-        break;
-      default:
-        fprintf(stderr, "unknown signal...\n");
+      }
+      break;
+    case SIGUSR2:
+      if(kill(0, SIGUSR2) < 0)
+      {
+        fprintf(stderr, "kill error on SIGUSR2\n");
         exit(-1);
-    }
-    // wait();
+      }
+      break;
+    default:
+      fprintf(stderr, "unknown signal...\n");
+      exit(-1);
   }
-  else sleep(3);
+  sleep(3);
+
 }
